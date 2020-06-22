ded004e8e6b4a2602975f0c4e6dab09d3163683e
kuniaki <k.ishikawa.0128@gmail.com>
2020-06-22 20:07:03 +0900

    create viewmodel

---
+++ app/src/main/java/com/fithub/roompractice/WordViewModel.kt
@@ -0,0 +1,25 @@
+package com.fithub.roompractice
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    val allWords: LiveData<List<Word>>
+
+    private val repository: WordRepository
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+}
