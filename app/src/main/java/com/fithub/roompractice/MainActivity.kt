036066b69451bfaff3cf3a040d965affc44a79e7
kuniaki <k.ishikawa.0128@gmail.com>
2020-06-23 12:47:13 +0900

    modify to observe allWords on MainActivity

--- app/src/main/java/com/fithub/roompractice/MainActivity.kt
+++ app/src/main/java/com/fithub/roompractice/MainActivity.kt
@@ -2,17 +2,29 @@ package com.fithub.roompractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordViewModel: WordViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Setup the RecyclerView
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         val adapter = WordListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Create the ViewModel and observe allWords
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+        wordViewModel.allWords.observe(this, Observer { words ->
+            words?.let { adapter.setWords(words) }
+        })
     }
 }
