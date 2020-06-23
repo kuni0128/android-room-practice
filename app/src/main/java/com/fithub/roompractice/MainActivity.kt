927f2e4a317854a280c3dcd7397ef13f992f94a2
kuniaki <k.ishikawa.0128@gmail.com>
2020-06-23 13:33:00 +0900

    modify to connect to NewWordActivity

--- app/src/main/java/com/fithub/roompractice/MainActivity.kt
+++ app/src/main/java/com/fithub/roompractice/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.fithub.roompractice
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var wordViewModel: WordViewModel
+    private val newWordActivityRequestCode = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +31,23 @@ class MainActivity : AppCompatActivity() {
         wordViewModel.allWords.observe(this, Observer { words ->
             words?.let { adapter.setWords(words) }
         })
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                wordViewModel.insert(Word(it))
+            }
+        } else {
+            Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_LONG).show()
+        }
     }
 }
